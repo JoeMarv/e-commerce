4023b138a094b7ebd4e541dd67c17ce161ef94a4
Joel <joepotential@gmail.com>
2022-09-16 09:44:58 +0100

    price added

--- app.js
+++ app.js
@@ -10,3 +10,19 @@ menuBtn.addEventListener('click', function() {
 closeBtn.addEventListener('click', function() {
     menu.classList.remove('open')
 })
+
+
+// CAROUSEL BUTTONS FUNCTIONS
+const prevBtn = document.querySelector('.prev-btn')
+const nextBtn = document.querySelector('.next-btn')
+const slides = document.querySelectorAll('.slide')
+
+slides.forEach(function(slide) {
+    nextBtn.addEventListener('click', function() {
+        slide.style.transform = 'translate(100vw)'
+    })
+
+    prevBtn.addEventListener('click', function() {
+        slide.style.transform = 'translate(-100vw)'
+    })
+})
