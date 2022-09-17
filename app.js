8d078897ac44cdedf91139daa9d57dc5fb221cf5
Joel <joepotential@gmail.com>
2022-09-17 21:24:36 +0100

    mobile view functional

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-// MENU TOGGLERS
+//DECLARATIONS
 const menuBtn = document.querySelector('.menu-btn button')
 const closeBtn = document.querySelector('.close-btn button')
 const menu = document.querySelector('.menu')
+const prevBtn = document.querySelector('.prev-btn')
+const nextBtn = document.querySelector('.next-btn')
+const slides = document.querySelectorAll('.slide')
+const carousel = document.querySelector('.carousel')
+const btns = document.querySelectorAll('.counter-btn')
+const counter = document.querySelector('.item-number')
+const addBtn = document.querySelector('.add-btn')
+const cartNumber = document.querySelector('.cart-number')
+const cartBtn = document.querySelector('.cart-btn')
+const cartContainer = document.querySelector('.cart-contents')
+const removeBtn = document.querySelector('.remove-btn')
+const qty = document.querySelector('.qty')
+const qtyPrice = document.querySelector('.qty-price')
 
+
+// MENU TOGGLERS
 menuBtn.addEventListener('click', function() {
     menu.classList.toggle('open')
 })
@@ -13,11 +28,6 @@ closeBtn.addEventListener('click', function() {
 
 
 // CAROUSEL BUTTONS FUNCTIONS
-const prevBtn = document.querySelector('.prev-btn')
-const nextBtn = document.querySelector('.next-btn')
-const slides = document.querySelectorAll('.slide')
-const carousel = document.querySelector('.carousel')
-
 slides.forEach(function(slide) {
     const slideWidth = slide.clientWidth
 
@@ -32,9 +42,6 @@ slides.forEach(function(slide) {
 
 
 //COUNTER
-const btns = document.querySelectorAll('.counter-btn')
-const counter = document.querySelector('.item-number')
-
 let count = 0
 btns.forEach(function(btn) {
     btn.addEventListener('click', function(e) {
@@ -62,20 +69,32 @@ btns.forEach(function(btn) {
 
 
 // ADD TO CART
-const addBtn = document.querySelector('.add-btn')
-const cartNumber = document.querySelector('.cart-number')
-
 addBtn.addEventListener('click', function() {
     cartNumber.textContent = counter.textContent
+    qty.textContent = counter.textContent
+    qtyPrice.textContent = '$'+ 125*counter.textContent
 
     if(cartNumber.textContent == 0) {
         cartNumber.classList.remove('show-number')
-    }
-
-    else{
+        cartContainer.classList.remove('filled')
+    }else{
         cartNumber.classList.add('show-number')
+        cartContainer.classList.add('filled')
     }
 })
 
 
-//CART CONTENTS
+//CART CONTENTS TOGGLE
+cartBtn.addEventListener('click', function() {
+    cartContainer.classList.toggle('show-cart')
+})
+
+
+//REMOVE BUTTON
+removeBtn.addEventListener('click', function() {
+    cartContainer.classList.remove('filled')
+    counter.textContent = 0
+    cartNumber.textContent = ''
+
+    console.log(counter);
+})
